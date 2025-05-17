4db848c842daa3bf358298ea4930361266f7cc6e
stefanRachev <starfystef@gmail.com>
2025-05-17 14:57:21 +0300

    feat: Add navigation links and instructions to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,34 @@
-
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-
-        <div className="w-full bg-amber-200">
-            <h1 className="text-red-200 text-2xl text-center">Home Page</h1>
+  return (
+    <div className="max-w-2xl mx-auto flex flex-col items-center p-4 bg-white">
+      <h1 className="font-bold text-center text-2xl mt-6">Home page</h1>
+      <div className="flex flex-col gap-2 mt-6 p-4 items-center">
+        <div className="flex flex-col gap-2 items-center">
+          <h2>To create a document, please log in to your account.</h2>
+          <Link
+            to="create-new-document"
+            className="bg-blue-200 rounded p-2 w-full text-center hover:bg-blue-400 transition duration-300"
+          >
+            Create Document
+          </Link>
         </div>
-    )
-}
+        <Link
+          to="/view-all-documents"
+          className="bg-green-200 rounded p-2 w-full text-center hover:bg-green-400 transition duration-300 mt-2"
+        >
+          View All Documents
+        </Link>
+        <Link
+          to="/search-document"
+          className="bg-green-200 rounded p-2 w-full text-center hover:bg-green-400 transition duration-300"
+        >
+          Search Document
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
